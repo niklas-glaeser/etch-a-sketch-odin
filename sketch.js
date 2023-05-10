8691b91e0d9878afed4dcadfede60a1ad7f19ed2
niklas-glaeser <123083964+niklas-glaeser@users.noreply.github.com>
2023-05-10 17:20:37 +0200

    adds the sketch function by leaving a pixelated trail

--- sketch.js
+++ sketch.js
@@ -1,35 +1,62 @@
 // Get the grid container element
-const container = document.getElementById('grid-container');
+const container = document.querySelector('.grid-container');
 
-// Define the dimensions of the grid
-const gridSize = 16;
+// Define the initial dimensions of the grid
+let gridSize = 16;
 
-// Apply flexbox properties to the container
-container.classList.add('grid-container');
+// Flag to track if the mouse button is clicked
+let isMouseDown = false;
 
-// Loop to create individual grid squares
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
-    // Create a new div element
-    const square = document.createElement('div');
-    
-    // Add a class to the square element
-    square.classList.add('grid-square');
-    
-    // Assign a random color to each square
-    square.style.backgroundColor = getRandomColor();
-    
-    // Append the square to the container
-    container.appendChild(square);
+// Function to create the grid
+function createGrid() {
+  // Clear previous grid
+  container.innerHTML = '';
+
+  // Calculate the square size based on container width
+  const containerWidth = 640;
+  const squareSize = containerWidth / gridSize;
+
+  // Loop to create individual grid squares
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      // Create a new div element
+      const square = document.createElement('div');
+
+      // Add a class to the square element
+      square.classList.add('grid-square');
+
+      // Set the square size
+      square.style.width = `${squareSize}px`;
+      square.style.height = `${squareSize}px`;
+
+      // Add event listeners for hover and click effects
+      square.addEventListener('mouseenter', () => {
+        if (isMouseDown) {
+          square.classList.add('hovered');
+        }
+      });
+
+      square.addEventListener('mousedown', () => {
+        square.classList.add('hovered');
+        isMouseDown = true;
+      });
+
+      square.addEventListener('mouseup', () => {
+        isMouseDown = false;
+      });
+
+      // Append the square to the container
+      container.appendChild(square);
+    }
   }
 }
 
-// Function to generate random color
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+// Call the createGrid function initially
+createGrid();
+
+// Button click event handler to reset the grid
+const resetButton = document.querySelector('button');
+resetButton.addEventListener('click', () => {
+  gridSize = parseInt(prompt('Enter grid size:'), 10);
+  createGrid();
+});
